49961e3e695b118ed5236a4f69174bfcf596b8a1
bsch <bsch.other@gmail.com>
2025-04-12 22:19:46 +0200

    added player drawing

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -10,14 +12,21 @@ def main():
     
     black = pygame.Color(0,0,0)
     background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+ 
         background.fill(black)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
